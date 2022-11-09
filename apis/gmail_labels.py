dd7410d9cb6ae5b8521f2a2aa96adbf19d4d342b
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-11-09 17:53:44 -0600

    gmail_labels module to handle the labels API

---
+++ apis/gmail_labels.py
@@ -0,0 +1,24 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from apis import google_auth as __auth
+from logger import __get_logger
+
+log = __get_logger(__name__)
+
+
+def get_id(label_name: str):
+    try:
+        creds = __auth.get_credentials()
+        service = build("gmail", "v1", credentials=creds)
+        results = service.users().labels().list(userId="me").execute()
+        labels = results.get("labels", [])
+        if not labels:
+            log.error("Labels not found")
+            return
+
+        label_found = filter(lambda label: label["name"] == label_name, labels)
+        return label_found.__next__()
+
+    except HttpError as error:
+        log.error(f"An error occurred: {error}")
+        return

cea1bbe911ca27536c770c32f0e1e33821f91c5a
fperez08 <32379040+fperez08@users.noreply.github.com>
2022-11-11 19:20:05 -0600

    Dev gmail get message content (#6)
    * Add code to test the get_raw_content function
    
    * Develop get raw content function
    
    This functions gets the raw content of an email encoded and returns
    the result as string encoded.
    
    Co-authored-by: Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ logger_list = [logging.getLogger("__main__"), logging.getLogger("apis")]
 def main():
     setup_global_logging(level=logging.DEBUG, loggers=logger_list)
     label = glabels.get_id("Python")
-    messages = gmessage.get_messages_id(max_results=2, label_id=label)
-    log.info(messages)
+    messages_id = gmessage.get_ids(max_results=2, label_id=label)
+    gmessage.get_raw_content(messages_id)
 
 
 if __name__ == "__main__":

6a30c0a3b3337bde544b264203b7546e022a40f3
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-11-15 17:32:36 -0600

    Update get_data and add get_email_data
    get_data was modified to search for data using a list of regex expresions
    and return the result if any
    
    get_email_data is a new function to return the data found by regex expresion
    from a list of emails

--- email_content.py
+++ email_content.py
@@ -19,9 +19,16 @@ def get_content(message: bytes, content_type: str = ""):
         return payload
 
 
-def get_data(text_message: str, regex: str):
-    result = re.search(regex, text_message)
+def get_data(text_message: str, regexs: list):
+    compiled_regex = re.compile("|".join(regexs))
+    result = re.findall(compiled_regex, text_message)
     if result:
-        return result.groups()
+        return result
     else:
         return result
+
+
+def get_email_data(messages: list, regexs: list):
+    messages_decoded = list(map(lambda m: decode_message(m), messages))
+    messages_content = list(map(lambda m: get_content(m), messages_decoded))
+    return list(map(lambda m: get_data(m, regexs), messages_content))

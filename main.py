4e646ebe99f3548ea022f6c39e29a4db561b1ee5
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-11-14 18:38:36 -0600

    Test the module email_content

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import logging
 
 from apis import gmail_labels as glabels, gmail_messages as gmessage
 from logger import setup_global_logging
+from email_content import decode_message, get_content, get_data
 
 log = logging.getLogger(__name__)
-logger_list = [logging.getLogger("__main__"), logging.getLogger("apis")]
+logger_list = [
+    logging.getLogger("__main__"),
+    logging.getLogger("apis"),
+]
 
 
 def main():
     setup_global_logging(level=logging.DEBUG, loggers=logger_list)
-    label = glabels.get_id("Python")
+    label = glabels.get_id("Withdrawal")
     messages_id = gmessage.get_ids(max_results=2, label_id=label)
-    gmessage.get_raw_content(messages_id)
+    messages = gmessage.get_raw_content(messages_id)
+    message_decoded = decode_message(messages[0])
+    content = get_content(message_decoded)
+    data = get_data(content, r"<b>\$(.+)<\/b>")
+    log.info(data)
 
 
 if __name__ == "__main__":

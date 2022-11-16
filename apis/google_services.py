2c88f63be813afc2e7a350015dfa07e69877ec41
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2022-11-16 17:57:09 -0600

    Rename the package and new service added
    This package was renamed with a better name and a function
    to get the spreedsheat API was added as well

---
+++ apis/google_services.py
@@ -0,0 +1,36 @@
+import logging
+import enum
+
+from googleapiclient.discovery import build
+from apis import google_auth as __auth
+from utils import memoize
+
+log = logging.getLogger(__name__)
+memoize_auth = memoize(__auth.get_credentials)
+
+
+class GmailResource(enum.Enum):
+    labels = 0
+    messages = 1
+
+
+def get_gmail(
+    resource: GmailResource,
+    service_name: str = "gmail",
+    version: str = "v1",
+):
+    creds = memoize_auth("credentials.json")
+    service = build(service_name, version, credentials=creds).users()
+
+    if resource.name == GmailResource.labels.name:
+        return service.labels()
+    elif resource.name == GmailResource.messages.name:
+        return service.messages()
+    else:
+        log.error(f"Incorrect gmail resource: {resource.name}")
+        return
+
+
+def get_spreadsheet(service_name: str = "sheets", version: str = "v4"):
+    creds = memoize_auth("credentials.json")
+    return build(service_name, version, credentials=creds).spreadsheets()

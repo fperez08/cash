ef388872052439f1ba765189abc17e27d9155c8c
fperez08 <32379040+fperez08@users.noreply.github.com>
2022-11-24 16:21:06 -0600

    dev read params (#14)
    * Ignore the new name of the config file
    
    * Update main to test the code
    
    * Functions to read params and construct the query
    
    A functions is added to define and read the params sent to main.py
    and the other functions use that params to build the gmail query
    
    Co-authored-by: Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import argparse
+
+
 def memoize(func):
     cache = dict()
 
@@ -9,3 +12,24 @@ def memoize(func):
         return result
 
     return memoized_func
+
+
+def get_params():
+    my_parser = argparse.ArgumentParser(allow_abbrev=False)
+    my_parser.add_argument("--label", action="store", type=str, required=True)
+    my_parser.add_argument("--days", action="store", type=str, required=True)
+
+    args = my_parser.parse_args()
+    arguments = args._get_kwargs()
+    return arguments[::-1]
+
+
+def get_gmail_query():
+    query = ""
+    for param in get_params():
+        key, value = param
+        if key != "days":
+            query = f"{key}:{value} "
+        else:
+            query = query + value
+    return query

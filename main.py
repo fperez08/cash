430459e7a9fa4815f0b77c739f104f00afd898a7
fperez08 <francisco.pgarcia@gmail.com>
2022-12-14 23:36:35 +0000

    Read the config path from an env variable

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from apis import gmail_messages as gmessage, google_sheets as sheets
 from logger import setup_global_logging
 from email_content import get_email_data
-from utils import get_gmail_query
+from utils import get_gmail_query, env
 
 log = logging.getLogger(__name__)
 logger_list = [
@@ -19,7 +19,7 @@ logger_list = [
 def main():
     setup_global_logging(level=logging.DEBUG, loggers=logger_list)
     withdrawal_query = get_gmail_query()
-    with open("cash_config.json", "r") as json_data_file:
+    with open(f"{env('CONFIG_PATH')}/cash_config.json", "r") as json_data_file:
         config = json.load(json_data_file)
         messages_id = gmessage.get_ids(query=withdrawal_query)
         messages = gmessage.get_raw_content(messages_id)
